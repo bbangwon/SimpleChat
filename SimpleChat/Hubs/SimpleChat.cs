eec19f231033811677719732c18b7f19ee43215a
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2022-05-29 21:18:39 +0900

    simple chat

---
+++ SimpleChat/Hubs/SimpleChat.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SimpleChat.Hubs
+{
+    public class SimpleChat : Hub
+    {
+        public async Task SendMessage(string user, string messgage)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, messgage);
+        }
+    }
+}
--- SimpleChat/Program.cs
+++ SimpleChat/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.MapRazorPages();
+app.MapHub<SimpleChat.Hubs.SimpleChat>("/simplechat");
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
